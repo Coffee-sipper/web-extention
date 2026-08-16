67388bdbc4af3a05c00f0c22b99cc1584bd1ebfc
Coffee-sipper <286983030+Coffee-sipper@users.noreply.github.com>
2026-08-17 00:10:17 +0300

    Implement lead storage and display feature
    Added functionality to store and display user input leads.

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 
-let btnEl= document.getElementById("input-btn");
+const btnEl= document.getElementById("input-btn");
+let myLeads=[];
+const inputEl= document.getElementById("input-el");
+const ulEl= document.getElementById("ul-el");
 btnEl.addEventListener("click", function(){
-  console.log("btn clicked hh");
+  myLeads.push(inputEl.value);
+  console.log(myLeads);
 });
+
+for (let i=0 ; i<myLeads.length; i++){
+  ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
+}
+
