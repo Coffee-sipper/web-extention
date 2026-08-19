9f4e7555278a23467b35e609dbe65d7c0affb4f9
Coffee-sipper <286983030+Coffee-sipper@users.noreply.github.com>
2026-08-19 13:25:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,55 +3,36 @@ const btnEl= document.getElementById("input-btn");
 let myLeads=[];
 const inputEl= document.getElementById("input-el");
 const ulEl= document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn");
 
 let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") );
 if (leadsFromLocalStorage){
   myLeads=leadsFromLocalStorage;
-  renderLeads();
+  render(myLeads);
 }
+
+deleteBtn.addEventListener("dblclick", function() {
+    console.log("double clicked!");
+    localStorage.clear();
+    myLeads = [];
+    render(myLeads);
+})
 
 btnEl.addEventListener("click", function(){
   myLeads.push(inputEl.value);
   inputEl.value="";
   localStorage.setItem("myLeads", JSON.stringify(myLeads) );
-  renderLeads();
+  render(myLeads);
 });
 
-function renderLeads(){
+function render(leads){
   let listItems="";
-  for (let i=0 ; i<myLeads.length; i++){
+  for (let i=0 ; i<leads.length; i++){
    //listItems += "<li> <a target='_blank'href='"+ myLeads[i]  +"'>" + myLeads[i]  + "</a></li>";
    listItems += `
             <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]}</a>
-            </li>
-    `
-  }
-
- ulEl.innerHTML = listItems;
-}
-const btnEl= document.getElementById("input-btn");
-let myLeads=[];
-const inputEl= document.getElementById("input-el");
-const ulEl= document.getElementById("ul-el");
-
-let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
-btnEl.addEventListener("click", function(){
-  myLeads.push(inputEl.value);
-  inputEl.value="";
-  localStorage.setItem("myLeads", JSON.stringify(myLeads) );
-  renderLeads();
-});
-
-function renderLeads(){
-  let listItems="";
-  for (let i=0 ; i<myLeads.length; i++){
-   //listItems += "<li> <a target='_blank'href='"+ myLeads[i]  +"'>" + myLeads[i]  + "</a></li>";
-   listItems += `
-            <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]}</a>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}</a>
             </li>
     `
   }
